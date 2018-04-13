8fb6c1dd84a2b3963f018a4c9fc63c536aa9752a
Igor Kantor <igor@devfire.io>
2018-04-13 15:07:56 -0400

    Create UpdateECSLaunchConfig.py

---
+++ UpdateECSLaunchConfig.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+
+import json
+import datetime
+import time
+import boto3
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    # get autoscaling client
+    client = boto3.client('autoscaling')
+
+    #define the ASG we are altering
+    targetASG = "ECS platformnonprod"
+    newAmiID = 'ami-28456852'
+    
+    # get object for the ASG we're going to update, filter by name of target ASG
+    #response = client.describe_auto_scaling_groups(AutoScalingGroupNames=[event['targetASG']])
+    response = client.describe_auto_scaling_groups(AutoScalingGroupNames=[targetASG])
+
+    if not response['AutoScalingGroups']:
+        return 'No such ASG'
+
+    # get name of InstanceID in current ASG that we'll use to model new Launch Configuration after
+    sourceInstanceId = response.get('AutoScalingGroups')[0]['Instances'][0]['InstanceId']
+
+    # create LC using instance from target ASG as a template, only diff is the name of the new LC and new AMI
+    timeStamp = time.time()
+    timeStampString = datetime.datetime.fromtimestamp(timeStamp).strftime('%Y-%m-%d:%H-%M-%S')
+    
+    #newLaunchConfigName = 'LC '+ event['newAmiID'] + ' ' + timeStampString
+    newLaunchConfigName = 'ECS-'+ newAmiID + '-' + timeStampString
+    
+    print ("New launch config name: " + newLaunchConfigName)
+    
+    #client.create_launch_configuration(
+    #    InstanceId = sourceInstanceId,
+    #    LaunchConfigurationName=newLaunchConfigName,
+    #    ImageId= event['newAmiID'] )
+
+    # update ASG to use new LC
+    # response = client.update_auto_scaling_group(AutoScalingGroupName = event['targetASG'],LaunchConfigurationName = newLaunchConfigName)
+
+    return 'Updated ASG `%s` with new launch configuration `%s` which includes AMI `%s`.' % (targetASG, newLaunchConfigName, newAmiID)
